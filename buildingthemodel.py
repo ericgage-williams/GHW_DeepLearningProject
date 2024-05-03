a37662b5108c33c81648212c93880d3a0ab40b51
Aidan Housenbold <HousenboldA@gmail.com>
2024-05-02 21:14:29 -0400

    to train ssh

---
+++ buildingthemodel.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+import torch
+import os
+from IPython.display import display, Image
+
+model = YOLO("yolov8m.pt")
+model.train(data="data.yaml", epochs=30)
+torch.save(model.state_dict, "save/model.pt")
